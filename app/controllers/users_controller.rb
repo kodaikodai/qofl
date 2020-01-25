bb72f12b595e2954173a5b92ac48429dc9e962d5
kodaikodai <k.sagawaa@gmail.com>
2020-01-25 15:37:13 +0900

    add pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @posts = user.posts
+    @posts = user.posts.page(params[:page]).per(5)
   end
 end
