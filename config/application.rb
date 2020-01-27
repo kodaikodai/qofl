1d94470e234a48cb15f6979f5f325d048d40c7d4
kodaikodai <k.sagawaa@gmail.com>
2020-01-27 16:30:59 +0900

    update リポジトリの名前変更

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Qofl
+module Change-Life
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
