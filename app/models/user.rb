22b435f6b17f0c50dbc84ae0ddc2bb3b2639e902
kodaikodai <k.sagawaa@gmail.com>
2020-02-03 20:33:08 +0900

    add モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+  validates :nickname, presence: true, length: { maximum: 9 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
