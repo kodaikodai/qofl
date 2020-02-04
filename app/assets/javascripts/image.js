3dcf970e6174f0990d2bc4e07211693f0c162733
kodaikodai <k.sagawaa@gmail.com>
2020-02-04 18:52:31 +0900

    Update image.js
    リロードしないとjsが機能しない問題に関して解決しました。

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,21 +1,23 @@
-$(function(){
-  fileField = $('#file')
-  // 選択された画像を取得し表示
-  fileField.on('change', fileField, function(e) {
-    var file = e.target.files[0]
-    var reader = new FileReader(),
-    $preview = $("#img-field");
-    reader.onload = (function(file) {
-      return function(e) {
-        $preview.empty();
-        $preview.append($('<img>').attr({
-          src: e.target.result,
-          width: "100%",
-          class: "preview",
-          title: file.name
-        }));
-      };
-    })(file);
-    reader.readAsDataURL(file);
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    fileField = $('#file')
+    // 選択された画像を取得し表示
+    fileField.on('change', fileField, function(e) {
+      var file = e.target.files[0]
+      var reader = new FileReader(),
+      $preview = $("#img-field");
+      reader.onload = (function(file) {
+        return function(e) {
+          $preview.empty();
+          $preview.append($('<img>').attr({
+            src: e.target.result,
+            width: "100%",
+            class: "preview",
+            title: file.name
+          }));
+        };
+      })(file);
+      reader.readAsDataURL(file);
+    });
   });
 });
