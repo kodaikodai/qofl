726103f17f8b9f2bf050dbe006f6710d969e9f4f
kodaikodai <k.sagawaa@gmail.com>
2020-03-01 21:43:11 +0900

    fix comment.js
    ターボリンクスにより、非同期通信が起動しない不具合を修正

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,36 +1,38 @@
-$(function(){
-  function buildHTML(comment){
-    var html = `<div class = "maincontents__comments__title__box">
-                  <div class = "maincontents__comments__title__box__name">
-                    <a href = /users/${comment.user_id} class = "maincontents__comments__title__box__name__link">${comment.user_name}</a>
-                    <span> :</span>
-                  </div>
-                  <div class = "maincontents__comments__title__box__text">
-                    ${comment.text}
-                  </div>
-                </div>`
-    return html;
-  }
-  $('#new_comment').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false,
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.maincontents__comments').append(html);
-      $('.form__textbox').val('');
-      $('.form__btn').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    function buildHTML(comment){
+      var html = `<div class = "maincontents__comments__title__box">
+                    <div class = "maincontents__comments__title__box__name">
+                      <a href = /users/${comment.user_id} class = "maincontents__comments__title__box__name__link">${comment.user_name}</a>
+                      <span> :</span>
+                    </div>
+                    <div class = "maincontents__comments__title__box__text">
+                      ${comment.text}
+                    </div>
+                  </div>`
+      return html;
+    }
+    $('#new_comment').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+      })
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.maincontents__comments').append(html);
+        $('.form__textbox').val('');
+        $('.form__btn').prop('disabled', false);
+      })
+      .fail(function(){
+        alert('error');
+      })
     })
   })
-})
+});
