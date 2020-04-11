ee1147629de0901efe9e12f390f00f6df8d78436
kodaikodai <k.sagawaa@gmail.com>
2020-04-11 15:58:22 +0900

    update post table

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,4 @@
 class Like < ApplicationRecord
+  belongs_to :post, counter_cache: :liles_count
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_11_064016) do
+ActiveRecord::Schema.define(version: 2020_04_11_065243) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_04_11_064016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "likes_count"
     t.index ["title"], name: "index_posts_on_title"
   end
 
