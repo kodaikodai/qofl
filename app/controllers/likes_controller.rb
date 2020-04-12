c21dc544433554986bb0843b320e33e3bcaf5535
kodaikodai <k.sagawaa@gmail.com>
2020-04-12 13:23:00 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,22 @@
 class LikesController < ApplicationController
+#-----いいねをする------
+  def create
+    # 現在のユーザーがこの投稿に対していいねする↓↓
+    @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    # この投稿のいいね数を全て代入↓↓
+    @likes = Like.where(post_id: params[:post_id])
+    # この投稿の情報を全て代入する↓↓
+    @posts = Post.all
+  end
+
+# -----いいねを解除する-----
+  def destroy
+    # 現在のユーザーのこの投稿に対してのいいねを代入↓↓
+    like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    like.destroy
+    # この投稿に対するいいねの情報を全て代入↓↓
+    @likes = Like.where(post_id: params[:post_id])
+    # この投稿の情報を全て代入する↓↓
+    @posts = Post.all
+  end
 end
