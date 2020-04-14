8451c9239aeb9a5332e7a3c6d1338b483526ab05
kodaikodai <k.sagawaa@gmail.com>
2020-04-14 15:28:12 +0900

    update code comment

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
 
 #-----いいねをする------
   def create
-    # 現在のユーザーがこの投稿に対していいねする↓↓
     @like = Like.create(user_id: current_user.id, post_id: params[:post_id])
     @like.save
     # いいね後のpost情報を代入↓↓
@@ -14,7 +13,6 @@ class LikesController < ApplicationController
 
 # -----いいねを解除する-----
   def destroy
-    # 現在のユーザーのこの投稿に対してのいいねを代入↓↓
     like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     like.destroy
     # いいね削除後のpost情報を代入↓↓
